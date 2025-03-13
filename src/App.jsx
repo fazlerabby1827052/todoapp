e0036fa2a53d7f98df7813f1ea3049b99adcef55
fazlerabby1827052 <rabby1827052@stud.kuet.ac.bd>
2025-03-13 09:09:41 +0600

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,29 @@ import "./App.css";
 import Card from "./card/Card";
 
 function App() {
-  // const arr = JSON.parse(localStorage.getItem("tasks")) || [];
+  const arr = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  // // const [arr2,setArr2]=useState(arr);
 
-  // // const [ar2,setAr2]=useState([]);
 
-  // const [ta1, setTa1] = useState(arr);
+  const [ta1, setTa1] = useState(arr);
 
-  // const handlesubmit = (e) => {
-  //   e.preventDefault();
-  //   // const ar=JSON.parse(localStorage.getItem("tasks")) || [];
+  const handlesubmit = (e) => {
+    e.preventDefault();
 
-  // const newTask = e.target.elements.addtodo.value.trim();
-  // if (newTask) {
-  //   setTa1([...ta1, newTask]);
-  // }
-  // e.target.elements.addtodo.value = "";
-  //   e.target.elements.addtodo.value = "";
-  // };
+  const newTask = e.target.elements.addtodo.value.trim();
+  if (newTask) {
+    setTa1([...ta1, newTask]);
+  }
+  e.target.elements.addtodo.value = "";
+  };
 
-  // useEffect(() => {
-  //   localStorage.setItem("tasks", JSON.stringify(ta1));
-  // }, [ta1]);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(ta1));
+  }, [ta1]);
 
   return (
     <>
-      {/* <form onSubmit={handlesubmit}>
+      <form onSubmit={handlesubmit}>
         <input
           type="text"
           name="addtodo"
@@ -43,7 +39,7 @@ function App() {
         <button type="submit">Add todo</button>
       </form>
       <br />
-      <div className="grid grid-cols-3 gap-2">
+      {/* <div className="grid grid-cols-3 gap-2">
         {ta1?.map((e, i) => (<div key={i} className="card card-dash shadow-2xl bg-sky-300 w-96">
               <div key={i} className="card-body">
                 {e}
@@ -55,7 +51,6 @@ function App() {
                       })
                     );
                   }}
-                  key={i}
                   className="btn btn-primary"
                 >
                   Delete
@@ -66,7 +61,7 @@ function App() {
         ))}
       </div> */}
 
-      <p>Working</p>
+      
     </>
   );
 }
